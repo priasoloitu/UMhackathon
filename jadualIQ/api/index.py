2271d3f57a63a29f54662d416525f1be3075ccaf
Darren772 <darrenmelvern27@gmail.com>
2026-04-25 10:04:41 +0700

    deleting vercel json, trying other method to deploy the website

--- jadualIQ/api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add backend directory to Python path
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-backend_dir = os.path.join(base_dir, "backend")
-sys.path.insert(0, backend_dir)
-
-from app import app
